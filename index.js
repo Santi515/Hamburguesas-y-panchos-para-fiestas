b76f8a345029f7f9d2a2c3a96dbede99e60d893e
santiago altagaveite <santiago.altagaveite@gmail.com>
2022-10-06 09:33:55 -0300

    Hubieron cambios en el codigo para poder calcular la compra de 3 distintas variables

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 let nombreProductoA = "Hamburguesas";
-let precioProductoA = 240;
+let precioProductoA = 180;
+let stockProductoA = 64;
 
 let nombreProductoB = "Panchos";
-let precioProductoB = 100;
+let precioProductoB = 120;
+let stockProductoB = 79;
 
-alert("Buenas tardes, si usted quiere adquirir nuestros servicios haga click en: Aceptar. Cada Hamburguesa saldra $240 y cada Pancho saldra $100");
+let nombreProductoC = "gaseosa";
+let precioProductoC = 150;
+let stockProductoC = 42;
 
-let cantidadProductoA = prompt("Ingrese cuantos" + " " + nombreProductoA + " " + "quiere comprar por favor");
-let cantidadProductoB = prompt("Ingrese cuantos" + " " + nombreProductoB + " " + "quiere comprar por favor");
+let productoCompra = prompt("Ingrese que producto quisiera comprar: \n1 - Hamburguesas \n2 - Panchos");
+let precioTotal = 0;
 
-let precioTotalA = cantidadProductoA * precioProductoA;
-let precioTotalB = cantidadProductoB * precioProductoB;
+if(productoCompra.toLowerCase() == 'hamburguesas'){
+    let cantidadProductoHamburguesas = prompt("Ingrese que cantidad de " + nombreProductoA + " desea comprar por favor.")
 
-let precioTotal = precioTotalA + precioTotalB;
+    if(cantidadProductoHamburguesas <= stockProductoA && cantidadProductoHamburguesas >= 0){
+        precioTotalA = cantidadProductoHamburguesas * precioProductoA;
+        alert("Seria un total de " + precioTotalA + " pesos.")
+    }
+    else{
+        alert("Disculpe, pero actualmente tenemos " + stockProductoA + " unidades de este producto.")
+    }
+}
+else if(productoCompra.toLowerCase() == 'panchos'){
+    let cantidadProductoPanchos = prompt("Ingrese que cantidad de " + nombreProductoB + " desea comprar por favor.")
 
-alert("El precio total es:" + " " + precioTotal)
+    if(cantidadProductoPanchos <= stockProductoB && cantidadProductoPanchos >= 0){
+        precioTotalB = cantidadProductoPanchos * precioProductoB;
+        alert("Seria un total de " + precioTotalB + " pesos.")
+    }
+    else{
+        alert("Disculpe, pero actualmente tenemos " + stockProductoB + " unidades de este producto.")
+    }
+}
+else if(productoCompra.toLowerCase() == 'gaseosa'){
+    let cantidadProductoGaseosas = prompt("Ingrese que cantidad de " + nombreProductoC + " desea comprar por favor.")
+
+    if(cantidadProductoGaseosas <= stockProductoC && cantidadProductoGaseosas >= 0){
+    precioTotalC = cantidadProductoGaseosas * precioProductoC;
+    alert("Seria un total de " + precioTotalC + " pesos.")
+    }
+    else{
+        alert("Disculpe, pero actualmente tenemos " + stockProductoC + " unidades de este producto.")
+    }
+}
+else{
+    alert("Disculpe, pero no tenemos ese producto a la venta.")
+}
+
+if(precioTotal != 0){
+    precioTotal = precioTotalA + precioTotalB + precioTotalC;
+
+    alert("El precio total de su compra serian " + precioTotal + " pesos.")
+}
